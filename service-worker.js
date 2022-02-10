61d3d328dedc4d7a887733cd216127118c99307a
Steve Signell <steve@frontierspatial.com>
2022-02-10 11:12:14 -0500

    Update service worker

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,9 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-  {url: 'index.html', revision: '12.07.21.2'},
-  {url: 'manifest.json', revision: '12.10.21.2'},
-  {url: 'assets/js/app.js', revision: '12.07.21.1'},
+  {url: 'index.html', revision: '2.08.22.1'},
+  {url: 'manifest.json', revision: '12.10.21.1'},
+  {url: 'assets/js/app.js', revision: '2.08.22.1'},
   {url: 'assets/css/app.css', revision: '10.01.21.1'},
   {url: 'assets/img/apple-touch-icon.png', revision: '10.01.21.1'},
   {url: 'assets/img/favicon-32x32.png', revision: '10.01.21.1'},
@@ -44,8 +44,8 @@ workbox.precaching.precacheAndRoute([
   {url: 'assets/vendor/leaflet-mbtiles/Leaflet.TileLayer.MBTiles.js', revision: '10.01.21.1'},
   {url: 'assets/vendor/csv2geojson-5.1.1/csv2geojson.min.js', revision: '11.05.21.2'},
   {url: 'data/trail_map.mbtiles', revision: '12.07.21.1'},
-  {url: 'data/points/points.csv', revision: '12.07.21.1'},
-  {url: 'data/points/photos/phenocam.jpg', revision: '12.07.21.1'}
+  {url: 'data/points.csv', revision: '2.07.22.1'},
+  {url: 'data/photos/phenocam.jpg', revision: '2.07.22.1'}
 ], {
   // Ignore all URL parameters.
   ignoreURLParametersMatching: [/.*/]
