5b24ec1872e344c05653783e26b80e3a62d53c26
Steve Signell <steve@frontierspatial.com>
2022-02-16 11:32:38 -0500

    updated basemap, service-worker

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,9 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-  {url: 'index.html', revision: '2.14.22.1'},
+  {url: 'index.html', revision: '2.16.22.1'},
   {url: 'manifest.json', revision: '2.10.22.1'},
-  {url: 'assets/js/app.js', revision: '2.14.22.1'},
+  {url: 'assets/js/app.js', revision: '2.16.22.1'},
   {url: 'assets/css/app.css', revision: '2.14.22.1'},
   {url: 'assets/img/apple-touch-icon.png', revision: '10.01.21.1'},
   {url: 'assets/img/favicon-32x32.png', revision: '10.01.21.1'},
@@ -32,6 +32,7 @@ workbox.precaching.precacheAndRoute([
   {url: 'assets/vendor/sqljs-1.6.2/sql-wasm.js', revision: '10.01.21.1'},
   {url: 'assets/vendor/sqljs-1.6.2/sql-wasm.wasm', revision: '10.01.21.1'},
   {url: 'assets/vendor/bootstrap-5.1.3/css/bootstrap.min.css', revision: '11.04.21.1'},
+  {url: 'assets/vendor/jquery-3.6.0.min.js', revision: '2.16.22.1'},
   {url: 'assets/vendor/bootstrap-5.1.3/js/bootstrap.bundle.min.js', revision: '11.04.21.1'},
   {url: 'assets/vendor/leaflet-1.7.1/images/layers.png', revision: '10.01.21.1'},
   {url: 'assets/vendor/leaflet-1.7.1/images/layers-2x.png', revision: '10.01.21.1'},
@@ -44,7 +45,7 @@ workbox.precaching.precacheAndRoute([
   {url: 'assets/vendor/leaflet-locatecontrol-0.74.0/L.Control.Locate.min.js', revision: '10.01.21.1'},
   {url: 'assets/vendor/leaflet-mbtiles/Leaflet.TileLayer.MBTiles.js', revision: '10.01.21.1'},
   {url: 'assets/vendor/csv2geojson-5.1.1/csv2geojson.min.js', revision: '11.05.21.2'},
-  {url: 'data/trail_map.mbtiles', revision: '2.10.22.1'},
+  {url: 'data/trail_map.mbtiles', revision: '2.16.22.1'},
   {url: 'data/points.csv', revision: '2.10.22.1'}
 ], {
   // Ignore all URL parameters.
